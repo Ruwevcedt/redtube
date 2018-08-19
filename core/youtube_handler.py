e699c43e02086b3ec11257a1d84900e53056ca43
윤준석 <yunjunseog@yunjunseog-ui-MacBook-Pro.local>
2018-08-20 02:06:51 +0900

    really really fixed download issue

--- core/youtube_handler.py
+++ core/youtube_handler.py
@@ -1,4 +1,4 @@
-from core import audio_handler
+from drawer import music_list
 import pytube
 import urllib
 import shutil
@@ -8,15 +8,15 @@ import os
 class VideoHandler:
 
     def __init__(self, url: str, audio_path="music/", thumbnail_path="picture/"):
-        self.audio_download_path = audio_path if os.getcwd().split('/')[-1] == "MAPP" else "../music/"
-        self.thumbnail_download_path = thumbnail_path if os.getcwd().split('/')[-1] == "MAPP" else "../picture/"
+        self.audio_download_path = audio_path
+        self.thumbnail_download_path = thumbnail_path
 
         self.youtube_object = pytube.YouTube(url)
-        self.title = self.youtube_object.title
+        self.title = "".join(filter(lambda x: x if x not in "~\"\'#%&*:<>?\/{|}" else False, self.youtube_object.title))
         self.audio_object = self.youtube_object.streams.filter(only_audio=True, file_extension='mp4').fmt_streams[0]
 
-    def _already_exists_check(self):
-        return True if os.path.exists(self.audio_download_path + self.title + ".aac") else False
+    def _already_exists_check(self, extention: str):
+        return True if os.path.exists(self.audio_download_path + self.title + "." + extention) else False
 
     def _download_audio(self):
         if self.audio_object:
@@ -27,20 +27,19 @@ class VideoHandler:
                                                                                               'hqdefault.jpg') else self.youtube_object.thumbnail_url
 
     def _download_thumbnail(self):
-        _reshaped_title = "".join(filter(lambda x: x if x not in "~\"#%&*:<>?\/{|}" else False, self.title))
         urllib.request.urlretrieve(self._thumbnail_resolution_check(),
-                                   f"{self.thumbnail_download_path}{_reshaped_title}.jpg")  # TODO : Make this Asyncable
+                                   f"{self.thumbnail_download_path}{self.title}.jpg")  # TODO : Make this Asyncable
 
     def _change_extention(self):
         file_name = self.audio_download_path + self.title
         shutil.move(file_name + ".mp4", file_name + ".aac")
 
     def download_sequence(self):
-        if not self._already_exists_check():
-            self._download_audio()
+        if not self._already_exists_check(extention="acc"):
+            self._download_audio() if not self._already_exists_check(extention="mp4") else False
             self._change_extention()
             self._download_thumbnail()
 
-            audio_handler.MusicList.update()
+            music_list.update()
         else:
             pass
