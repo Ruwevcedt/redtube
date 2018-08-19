5715cbbb4fa283f51d59a0721141c6f292d4010b
윤준석 <yunjunseog@yunjunseog-ui-MacBook-Pro.local>
2018-08-20 01:19:10 +0900

    fixing download issues...

--- drawer/index.py
+++
@@ -1,7 +0,0 @@
-from drawer import app, redirect, url_for, login_required
-
-
-@app.route('/')
-@login_required
-def gate():
-    return redirect(url_for('player'))
--- drawer/picture.py
+++
@@ -1,7 +0,0 @@
-from drawer import app, send_file, login_required, music_list
-
-
-@app.route('/picture/<image_name>')
-@login_required
-def send_image(image_name):
-    return send_file(music_list.search(image_name).capture)
