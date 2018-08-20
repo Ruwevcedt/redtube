141d1ff018c7a0b6395c5dd6e81f3be4e8cb82de
윤준석 <yunjunseog@yunjunseog-ui-MacBook-Pro.local>
2018-08-20 22:04:36 +0900

    super minor update

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from gevent import pywsgi
 from drawer import app
 
 
-http_server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
+http_server = pywsgi.WSGIServer(('0.0.0.0', 19283), app)
 http_server.serve_forever()
