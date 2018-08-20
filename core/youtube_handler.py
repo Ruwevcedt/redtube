beee11f4747d3d26d66b52e5a76c3020f4d376a6
Ruwevcedt <yjs96624@gmail.com>
2018-08-20 15:43:50 +0900

    Update youtube_handler.py

--- core/youtube_handler.py
+++ core/youtube_handler.py
@@ -16,8 +16,8 @@ class VideoHandler:
         self.title = "".join(filter(lambda x: x if x not in "~\"\'#%&*:<>?\/{|}" else False, self.youtube_object.title))
         self.audio_object = self.youtube_object.streams.filter(only_audio=True, file_extension='mp4').fmt_streams[0]
 
-    def _already_exists_check(self, extention: str):
-        return True if os.path.exists(self.audio_download_path + self.title + extention) else False
+    def _already_exists_check(self, extension: str):
+        return True if os.path.exists(self.audio_download_path + self.title + extension) else False
 
     def _download_audio(self):
         if self.audio_object:
@@ -36,8 +36,8 @@ class VideoHandler:
         shutil.move(file_name + ".mp4", file_name + ".aac")
 
     def download_sequence(self):
-        if not self._already_exists_check(extention=".aac"):
-            self._download_audio() if not self._already_exists_check(extention=".mp4") else False
+        if not self._already_exists_check(extension=".aac"):
+            self._download_audio() if not self._already_exists_check(extension=".mp4") else False
             self._change_extention()
             self._download_thumbnail()
             music_list.update()
