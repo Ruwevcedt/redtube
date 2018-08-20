3d64104bd924a30f7c4adadf36959b3252581239
윤준석 <yunjunseog@yunjunseog-ui-MacBook-Pro.local>
2018-08-20 15:39:32 +0900

    made  something asyncable

--- core/youtube_handler.py
+++ core/youtube_handler.py
@@ -3,6 +3,7 @@ import pytube
 import urllib
 import shutil
 import os
+import threading
 
 
 class VideoHandler:
@@ -16,26 +17,27 @@ class VideoHandler:
         self.audio_object = self.youtube_object.streams.filter(only_audio=True, file_extension='mp4').fmt_streams[0]
 
     def _already_exists_check(self, extention: str):
-        return True if os.path.exists(self.audio_download_path + self.title + "." + extention) else False
+        return True if os.path.exists(self.audio_download_path + self.title + extention) else False
 
     def _download_audio(self):
         if self.audio_object:
             self.audio_object.download(self.audio_download_path)
 
     def _thumbnail_resolution_check(self):
-        return self.youtube_object.thumbnail_url if self.youtube_object.thumbnail_url.replace('defualt.jpg', 'hqdefault.jpg') else self.youtube_object.thumbnail_url
+        _higher_resolution = self.youtube_object.thumbnail_url.replace('default.jpg', 'hqdefault.jpg')
+        return _higher_resolution if _higher_resolution else self.youtube_object.thumbnail_url
 
     def _download_thumbnail(self):
         urllib.request.urlretrieve(self._thumbnail_resolution_check(),
-                                   f"{self.thumbnail_download_path}{self.title}.jpg")  # TODO : Make this Asyncable
+                                   f"{self.thumbnail_download_path}{self.title}.jpg")
 
     def _change_extention(self):
         file_name = self.audio_download_path + self.title
         shutil.move(file_name + ".mp4", file_name + ".aac")
 
     def download_sequence(self):
-        if not self._already_exists_check(extention="aac"):
-            self._download_audio() if not self._already_exists_check(extention="mp4") else False
+        if not self._already_exists_check(extention=".aac"):
+            self._download_audio() if not self._already_exists_check(extention=".mp4") else False
             self._change_extention()
             self._download_thumbnail()
             music_list.update()
--- drawer/charge.py
+++ drawer/charge.py
@@ -1,14 +1,17 @@
 from drawer import app, request, login_required, render_template, redirect, url_for
 from core import youtube_handler, audio_handler
+import threading
 
 
 @app.route('/charge', methods=['GET', 'POST'])
 @login_required
 def charge():
 
-    # FIXME
+    def download(url: str):
+        youtube_handler.VideoHandler(url=url).download_sequence()
 
     if request.method == 'POST':
-        youtube_handler.VideoHandler(url=request.form['youtube_link']).download_sequence()
+        worker = threading.Thread(target=download, args=(request.form['youtube_link'],))
+        worker.start()
         return redirect(url_for('player'))
     return render_template('charge.html')
