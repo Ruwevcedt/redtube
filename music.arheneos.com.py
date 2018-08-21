1da58bc00fabdefe8b169a330d9d03dce35a448a
윤준석 <yunjunseog@yunjunseog-ui-MacBook-Pro.local>
2018-08-21 21:42:04 +0900

    added https setting

--- music.arheneos.com.py
+++ music.arheneos.com.py
@@ -1,6 +1,13 @@
 from gevent import pywsgi
 from drawer import app
+import sys
 
+keyfile = ""
+certfile = ""
 
-http_server = pywsgi.WSGIServer(('0.0.0.0', 19283), app)
+if sys.platform == "darwin":
+    http_server = pywsgi.WSGIServer(('0.0.0.0', 2048), app)
+else:
+    http_server = pywsgi.WSGIServer(('0.0.0.0', 2048), app, keyfile=keyfile, certfile=certfile)
+
 http_server.serve_forever()
